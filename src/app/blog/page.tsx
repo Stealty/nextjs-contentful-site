4cb1b7aa521508e4c629e02f7012214c2878071d
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-01-18 11:00:12 -0300

    initial setup with contentful integration

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import { createClient } from "contentful";
+
+export default async function Page() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  });
+
+  const product = await client.getEntries("post");
+  console.log(product);
+  return <h1>Blog</h1>;
+}
