0f6a5a1844dd7db10dbca08a7ccbd91b40de8f00
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-01-20 00:22:59 -0300

    fix - about, pricing and blog pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,3 @@
-import Header from "@/components/organisms/Header/Header";
-import { createClient } from "contentful";
-
 export default async function Page() {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID as string,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
-  });
-
-  const product = await client.getEntries("post");
-
   return <div></div>;
 }
