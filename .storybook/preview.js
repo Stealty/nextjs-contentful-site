3fca940b4e850e34480598520aa93eaa7b6be8c6
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-01-24 14:56:34 -0300

    feat - storybook dark theme and next/image fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,11 @@
 import "../src/styles/variables.css";
 import "../src/styles/globals.css";
+import * as NextImage from "next/image";
 
 export const parameters = {
+  darkMode: {
+    current: "dark",
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
@@ -10,3 +14,17 @@ export const parameters = {
     },
   },
 };
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+      // this is new!
+      blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
+    />
+  ),
+});
