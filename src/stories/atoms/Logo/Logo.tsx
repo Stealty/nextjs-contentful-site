aa198dec5028c3ec94f76c403aa25e452a6471f7
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-01-24 14:57:36 -0300

    feat - placeholder blur

--- src/stories/atoms/Logo/Logo.tsx
+++ src/stories/atoms/Logo/Logo.tsx
@@ -7,5 +7,13 @@ type ImageProps = {
   alt: string;
 };
 export default function Logo({ source, alt, width, height }: ImageProps) {
-  return <Image src={source} alt="Agency logo" width={width} height={height} />;
+  return (
+    <Image
+      src={source}
+      alt="Agency logo"
+      width={width}
+      height={height}
+      placeholder="blur"
+    />
+  );
 }
